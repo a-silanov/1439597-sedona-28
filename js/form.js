8299bf5d826dc5c6bd273ea43223ed083c863c68
a-silanov <a-silanov@users.noreply.github.com>
2020-07-01 17:38:38 +0200

    fix

--- js/form.js
+++ js/form.js
@@ -1,7 +1,14 @@
 var link = document.querySelector(".search-hotel");
 var popup = document.querySelector(".search-container");
+var arrivingDate = popup.querySelector("[name=date]");
+var form = popup.querySelector("search-form"); 
 
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.toggle("modal-show");
+  arrivingDate.focus();
+});
+
+form.addEventListener("submit", function () {
+  console.log("Отправляем форму");
 });
