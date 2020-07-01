b30f96077b8d90a5551ad92ebfe317c02b8d2409
a-silanov <a-silanov@users.noreply.github.com>
2020-07-01 21:51:39 +0200

    fix

--- js/form.js
+++ js/form.js
@@ -2,19 +2,17 @@ var link = document.querySelector(".search-hotel");
 var popup = document.querySelector(".search-container");
 var arrivingDate = popup.querySelector(".arriving");
 var departureDate = popup.querySelector(".departure");
-var form = popup.querySelector(".search-form"); 
-
-link.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popup.classList.toggle("modal-show");
-  arrivingDate.focus();
+var form = popup.querySelector(".search-form");
+link.addEventListener("click", function(evt) {
+	evt.preventDefault();
+	popup.classList.toggle("modal-show");
+	arrivingDate.focus();
 });
-
-form.addEventListener("submit", function (evt) {
-  if (!arrivingDate.value || !departureDate.value) {
-  evt.preventDefault();
-  popup.classList.add("modal-error");
-  }
+form.addEventListener("submit", function(evt) {
+	if (!arrivingDate.value || !departureDate.value) {
+		evt.preventDefault();
+		popup.classList.add("modal-error");
+	}
 });
 
 
